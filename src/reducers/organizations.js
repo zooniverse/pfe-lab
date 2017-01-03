f68658156a7415ae94a7c4f3e1f64b5261500924
Amy Boyer <amy@zooniverse.org>
2017-01-03 11:34:53 -0600

    this reducer combinator will automatically perform Object.assign on state updates and it will also warn if multiple reducers match the same action

--- src/reducers/organizations.js
+++ src/reducers/organizations.js
@@ -4,13 +4,9 @@ import initialState from './initial-state';
 export default function (state = initialState, action) { // eslint-disable-line import/prefer-default-export
   switch (action.type) {
     case types.SET_CURRENT_ORGANIZATION:
-      return Object.assign({}, state, {
-        organization: action.organization,
-      });
+      return { organization: action.organization };
     case types.SET_ORGANIZATIONS:
-      return Object.assign({}, state, {
-        organizations: action.organizations,
-      });
+      return { organizations: action.organizations };
     default:
       return state;
   }
