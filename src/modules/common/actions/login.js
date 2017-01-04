43704df5e214a6b9c2e82718e843b942ea7a915b
Amy Boyer <amy@zooniverse.org>
2017-01-04 13:44:45 -0600

    fix action creators in login.js

--- src/modules/common/actions/login.js
+++ src/modules/common/actions/login.js
@@ -2,21 +2,17 @@ import oauth from 'panoptes-client/lib/oauth';
 import * as types from '../../../constants/action-types';
 
 export function setLoginUser(user) {
-  return (dispatch) => {
-    dispatch({
-      type: types.SET_LOGIN_USER,
-      user,
-    });
+  return {
+    type: types.SET_LOGIN_USER,
+    user,
   };
 }
 
-export function checkLoginUser() {  // First thing on app load - check if the user is logged in.
-  return (dispatch) => {
-    oauth.checkCurrent()
-      .then((user) => {
-        dispatch(setLoginUser(user));
-      });
-  };
+export function checkLoginUser(dispatch) {
+  oauth.checkCurrent()
+    .then((user) => {
+      dispatch(setLoginUser(user));
+    });
 }
 
 function computeRedirectURL(window) {
@@ -24,15 +20,13 @@ function computeRedirectURL(window) {
     `${window.location.protocol}//${window.location.hostname}:${window.location.port}`;
 }
 
-export function loginToPanoptes() {  // Returns a login page URL for the user to navigate to.
-  return (() => oauth.signIn(computeRedirectURL(window)));
+export function loginToPanoptes() {
+  oauth.signIn(computeRedirectURL(window));
 }
 
-export function logoutFromPanoptes() {
-  return (dispatch) => {
-    oauth.signOut()
-      .then((user) => {
-        dispatch(setLoginUser(user));
-      });
-  };
+export function logoutFromPanoptes(dispatch) {
+  oauth.signOut()
+    .then((user) => {
+      dispatch(setLoginUser(user));
+    });
 }
