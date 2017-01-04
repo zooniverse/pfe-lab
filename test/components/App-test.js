16a006e57bf37f039f22cb300d794ae42c29e112
Amy Boyer <amy@zooniverse.org>
2017-01-04 11:38:47 -0600

    write a proper test for the app container

--- test/components/App-test.js
+++ test/components/App-test.js
@@ -1,23 +1,15 @@
-// import React from 'react';
+/* eslint-disable react/jsx-filename-extension, no-unused-expressions */
+
+import React from 'react';
 import { expect } from 'chai';
-// import TestUtils from 'react-addons-test-utils';
+import { shallow } from 'enzyme';
+
 import App from '../../src/modules/common/containers/App';
 
 
 describe('App', () => {
-  // this test is no longer relevant because a shallow render produces
-  // child components
-
-  // const shallowRenderer = TestUtils.createRenderer();
-  // shallowRenderer.render(<App />); // eslint-disable-line react/jsx-filename-extension
-  // const app = shallowRenderer.getRenderOutput();
-  // it('should have a div as container', () => {
-  //   expect(app.type).to.equal('div');
-  // });
-
-  it('should return something', () => {
-    const returnSomething = App.prototype.returnSomething('hello!');
-
-    expect(returnSomething).to.be.equal('hello!');
+  it('should load the layout and connect it to the redux store', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('Connect(Layout)')).to.have.length(1);
   });
 });
