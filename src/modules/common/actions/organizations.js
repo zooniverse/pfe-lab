cf99369aedad5605acae5fee7bb851b72b1afa55
Amy Boyer <amy@zooniverse.org>
2017-01-04 13:51:01 -0600

    remove unneeded linter advice

--- src/modules/common/actions/organizations.js
+++ src/modules/common/actions/organizations.js
@@ -1,13 +1,13 @@
 import * as types from '../../../constants/action-types';
 
-export function setCurrentOrganization(organization) { // eslint-disable-line import/prefer-default-export
+export function setCurrentOrganization(organization) {
   return {
     type: types.SET_CURRENT_ORGANIZATION,
     organization,
   };
 }
 
-export function setOrganizations(organizations) { // eslint-disable-line import/prefer-default-export
+export function setOrganizations(organizations) {
   return {
     type: types.SET_ORGANIZATIONS,
     organizations,
