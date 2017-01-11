eb0291aeb74ed1add34fadadf3753f66bca993a2
Amy Boyer <amy@zooniverse.org>
2017-01-11 16:06:17 -0600

    got a working test for the login process

--- test/services/panoptes-test.js
+++ test/services/panoptes-test.js
@@ -1,6 +1,8 @@
 import { expect } from 'chai';
 import nock from 'nock';
 
+import oauth from 'panoptes-client/lib/oauth';
+
 import * as actionTypes from '../../src/constants/action-types';
 import * as panoptes from '../../src/services/panoptes';
 
@@ -70,6 +72,35 @@ describe('Panoptes', () => {
           done(ex);
         }
       });
+    });
+  });
+
+  xdescribe('with no user', function (done) {
+    let lastAction = null;
+    nock('https://panoptes-staging.zooniverse.org')
+      .get(/^\/oauth\/authorize/)
+      .reply(302, '', {
+        'Cache-Control': 'no-cache',
+        'location': 'https://panoptes-staging.zooniverse.org/users/sign_in',
+        'Strict-Transport-Security': 'max-age=31536000; includeSubDomains',
+        'X-Frame-Options': 'SAMEORIGIN',
+        'X-XSS-Protection': '1; mode=block',
+      });
+
+    nock('https://panoptes-staging.zooniverse.org')
+      .get(/^\/users\/sign_in/)
+      .reply(200, '', {
+        'x-csrf-token': 'abcdefghijk',
+      });
+
+    panoptes.checkLoginUser((action) => { lastAction = action; }).then(() => {
+      done();
+    });
+
+    it('should know that nobody is logged in', function () {
+      expect(lastAction).to.not.be.null;
+      expect(lastAction.type).to.equal(actionTypes.SET_LOGIN_USER);
+      expect(lastAction.user).to.be.null;
     });
   });
 });
