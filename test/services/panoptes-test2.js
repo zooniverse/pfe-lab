56150a7f5bca15ff1f30198a97d7b6c6a582be42
Amy Boyer <amy@zooniverse.org>
2017-01-12 10:56:22 -0600

    both tests work independently but can't be run together

--- test/services/panoptes-test2.js
+++ test/services/panoptes-test2.js
@@ -4,18 +4,17 @@ import nock from 'nock';
 import * as actionTypes from '../../src/constants/action-types';
 import * as panoptes from '../../src/services/panoptes';
 
-import { user } from '../modules/users/test-data';
-
 const stagingHost = 'https://panoptes-staging.zooniverse.org';
 
 describe('Panoptes', () => {
-  describe('with a valid user', function (done) {
+  describe('with no user', function (done) {
     let lastAction = null;
 
     const scope = nock(stagingHost)
       .get(/^\/oauth\/authorize/)
       .reply(302, '', {
-        'location': 'https://localhost:3000',
+        'Cache-Control': 'no-cache',
+        'location': 'https://panoptes-staging.zooniverse.org/users/sign_in',
         'Strict-Transport-Security': 'max-age=31536000; includeSubDomains',
         'X-Frame-Options': 'SAMEORIGIN',
         'X-XSS-Protection': '1; mode=block',
@@ -23,21 +22,17 @@ describe('Panoptes', () => {
 
     scope
       .get(/^\/api\/me/)
-      .reply(200, {
-        users: [user],
-      });
+      .reply(401);
 
     panoptes.checkLoginUser((action) => { lastAction = action; }).then(() => {
       nock.removeInterceptor(scope);
       done();
     });
 
-    it('should know when somebody is logged in', function () {
+    it('should know that nobody is logged in', function () {
       expect(lastAction).to.not.be.null;
       expect(lastAction.type).to.equal(actionTypes.SET_LOGIN_USER);
-      expect(lastAction.user).to.not.be.null;
-      expect(lastAction.user.id).to.equal(user.id);
-      expect(lastAction.user.login).to.equal(user.login);
+      expect(lastAction.user).to.be.null;
     });
   });
 });
