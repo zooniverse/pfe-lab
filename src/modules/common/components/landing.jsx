9a0a390c1a0140febc0bab426520b2b221e458d6
mcbouslog <mcbouslog@gmail.com>
2017-01-24 10:19:33 -0600

    update style-guide css

--- src/modules/common/components/landing.jsx
+++ src/modules/common/components/landing.jsx
@@ -1,25 +1,22 @@
 import React from 'react';
 
 const Landing = () =>
-  <div className="content-section">
+  <div className="landing">
     <section>
       <h1>H1 Heading is big!</h1>
       <h2>Landing</h2>
       <h3>Why not an H3?</h3>
       <p>Lab blerb goes on and on and on and on. Ok is this enough? Probably not, but ok.</p>
     </section>
-    <section style={{ background: '#CBCCCB' }}>
-      <div style={{ padding: '50px' }}>
+    <section className="landing__buttons-section">
+      <div className="landing__buttons-container">
         <button className="button button--ghost-white">Sign-In</button>
-        <div style={{ marginTop: '50px' }} />
         <button className="button button--ghost-navy">Back to Classifying</button>
-        <div style={{ marginTop: '50px' }} />
         <button className="button button--ghost-teal">Button</button>
-        <div style={{ marginTop: '50px' }} />
+      </div>
+      <div className="landing__buttons-container">
         <button className="button button--full-white">Button</button>
-        <div style={{ marginTop: '50px' }} />
         <button className="button button--full-teal">Button</button>
-        <div style={{ marginTop: '50px' }} />
         <button className="button button--full-alert">Button</button>
       </div>
     </section>
