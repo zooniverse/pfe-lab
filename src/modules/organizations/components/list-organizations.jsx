1bac0ad5cbbc0b721ca3ecbd4ead7a3865c9f1e3
mcbouslog <mcbouslog@gmail.com>
2017-02-21 16:33:05 -0600

    edit list-orgs component

--- src/modules/organizations/components/list-organizations.jsx
+++ src/modules/organizations/components/list-organizations.jsx
@@ -12,22 +12,37 @@ const ListOrganizations = ({ ownedOrganizations, collaboratorOrganizations }) =>
 
   return (
     <div>
-      <h2>Organizations</h2>
-      <h3>Owned</h3>
+      <div>
+        <h3>
+          <strong>Your organizations</strong>
+        </h3>
+        <ul>
+          {ownedOrganizations.map(organization => (
+            <li key={organization.id}>
+              <div>
+                <Link to="/">
+                  <img src={organization.avatar.src} alt="" />
+                  <div>
+                    <strong>{organization.display_name}</strong>
+                  </div>
+                  <span>
+                    <i className="fa fa-pencil fa-fw" />
+                    <small>Edit</small>
+                  </span>
+                </Link>
+                <Link to="/" className="">
+                  <i className="fa fa-hand-o-right fa-fw" />
+                  <small>View</small>
+                </Link>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <h3>Collaborations</h3>
       <dl>
-        { ownedOrganizations.map(organization => (
-          <span key={organization.id}>
-            <dt><Link to={`/organizations/${organization.id}`}>{organization.display_name}</Link></dt>
-            <dd>{organization.description}</dd>
-            <dd>
-              <img alt="test" src={organization.avatar.src} />
-            </dd>
-          </span>
-        ))}
-      </dl>
-      <h3>Collaborator</h3>
-      <dl>
-        { collaboratorOrganizations.map(organization => (
+        {collaboratorOrganizations.map(organization => (
           <span key={organization.id}>
             <dt><Link to={`/organizations/${organization.id}`}>{organization.display_name}</Link></dt>
             <dd>{organization.description}</dd>
