c5cb9dcd185c8e621fe01f658e54bff353ebaee4
Mark Bouslog <mcbouslog@gmail.com>
2017-02-21 23:17:57 -0600

    create consts for list-group and list-row, init styling

--- src/modules/organizations/components/list-organizations.jsx
+++ src/modules/organizations/components/list-organizations.jsx
@@ -1,7 +1,42 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-import { organizationsShape } from '../model';
+import { organizationsShape, organizationShape } from '../model';
+
+const ListRow = ({ organization }) =>
+  <div className="list__row">
+    <Link to={`/organizations/${organization.id}`} className="list__edit list--action">
+      {organization.avatar &&
+        <img src={organization.avatar.src} alt="avatar" className="list__avatar" />}
+      <div className="list__description">
+        <strong>{organization.display_name}</strong>{' '}
+        {organization.ownerRole &&
+          <small>
+            by User #{organization.ownerRole.links.owner.id}
+          </small>}
+      </div>
+      <span className="list__icon">
+        <i className="fa fa-pencil fa-fw" />
+        <small>Edit</small>
+      </span>
+    </Link>
+    <Link to="/" className="list__icon list--action">
+      <i className="fa fa-hand-o-right fa-fw" />
+      <small>View</small>
+    </Link>
+  </div>;
+
+const ListGroup = ({ organizations, title }) =>
+  <div>
+    <p className="list__title">{title}</p>
+    <ul className="list__list">
+      {organizations.map(organization => (
+        <li key={organization.id} className="list__item">
+          <ListRow organization={organization} />
+        </li>
+      ))}
+    </ul>
+  </div>;
 
 const ListOrganizations = ({ ownedOrganizations, collaboratorOrganizations }) => {
   if (!ownedOrganizations || !collaboratorOrganizations) {
@@ -11,47 +46,31 @@ const ListOrganizations = ({ ownedOrganizations, collaboratorOrganizations }) =>
   }
 
   return (
-    <div>
-      <div>
-        <h3>
-          <strong>Your organizations</strong>
-        </h3>
-        <ul>
-          {ownedOrganizations.map(organization => (
-            <li key={organization.id}>
-              <div>
-                <Link to="/">
-                  <img src={organization.avatar.src} alt="" />
-                  <div>
-                    <strong>{organization.display_name}</strong>
-                  </div>
-                  <span>
-                    <i className="fa fa-pencil fa-fw" />
-                    <small>Edit</small>
-                  </span>
-                </Link>
-                <Link to="/" className="">
-                  <i className="fa fa-hand-o-right fa-fw" />
-                  <small>View</small>
-                </Link>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
-
-      <h3>Collaborations</h3>
-      <dl>
-        {collaboratorOrganizations.map(organization => (
-          <span key={organization.id}>
-            <dt><Link to={`/organizations/${organization.id}`}>{organization.display_name}</Link></dt>
-            <dd>{organization.description}</dd>
-            <dd>by User #{organization.ownerRole.links.owner.id}</dd>
-          </span>
-        ))}
-      </dl>
+    <div className="list">
+      <ListGroup organizations={ownedOrganizations} title={'Your Organizations'} />
+      <p className="list__button-container">
+        <button
+          type="button"
+          className="button list__create-button"
+          onClick={() => console.log('create an org!')}
+        >
+          Create a new project
+        </button>{' '}
+        <Link to="/" className="button list__info-button">How-to</Link>{' '}
+      </p>
+      <hr />
+      <ListGroup organizations={collaboratorOrganizations} title={'Collaborations'} />
     </div>
   );
+};
+
+ListRow.propTypes = {
+  organization: organizationShape,
+};
+
+ListGroup.propTypes = {
+  organizations: organizationsShape,
+  title: React.PropTypes.string,
 };
 
 ListOrganizations.propTypes = {
