fc6dc2672f1a08915274e4e203edae626606b59d
Will Granger <twgranger@gmail.com>
2017-03-02 10:56:48 -0600

    Adjust App Test and Write Admin Tests

--- test/modules/common/App-test.js
+++ test/modules/common/App-test.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
+import configureStore from 'redux-mock-store';
+import initialState from '../../../src/initial-state';
 
 import App from '../../../src/modules/common/containers/App';
 
 
 describe('App', () => {
   it('should load the layout and connect it to the redux store', () => {
-    const wrapper = shallow(<App />);
+    const mockStore = configureStore([]);
+    const store = mockStore(initialState);
+
+    const wrapper = shallow(<App store={store} />).shallow();
     expect(wrapper.find('Connect(Layout)')).to.have.length(1);
   });
 });
