462091013751e70dd584f857964fd607156705fe
srallen <srallen@users.noreply.github.com>
2017-05-23 10:02:53 -0500

    Update markdownz and add to details editor (#50)

--- src/modules/organizations/containers/details-form-container.jsx
+++ src/modules/organizations/containers/details-form-container.jsx
@@ -13,14 +13,32 @@ class DetailsFormContainer extends React.Component {
 
     this.collectValues = this.collectValues.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.onTextAreaChange = this.onTextAreaChange.bind(this);
     this.resetOrganization = this.resetOrganization.bind(this);
+
+    this.state = {
+      textarea: ''
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ textarea: this.props.organization.introduction });
+  }
+
+  onTextAreaChange(event) {
+    const textarea = event.target.value;
+
+    this.setState({ textarea });
   }
 
   collectValues() {
+    // TODO rework this to better work with the MarkdownEditor
+    // TODO only submit changed fields!
     const result = {};
     Object.keys(this.fields).forEach((fieldName) => {
       result[fieldName] = this.fields[fieldName].value();
     });
+    result.introduction = this.state.textarea;
     return result;
   }
 
@@ -36,6 +54,8 @@ class DetailsFormContainer extends React.Component {
   render() {
     // TODO rename prop in markdownz to be resource not project.
     // TODO extract <MarkdownHelp /> into shared components repo.
+    // TODO extract MarkdownEditor into its own component put into common folder
+    // TODO split into functional component
     const organization = this.props.organization;
     const NameInput = bindInput(organization.display_name, <input type="text" />);
     const DescriptionInput = bindInput(organization.description, <input type="text" />);
@@ -59,6 +79,25 @@ class DetailsFormContainer extends React.Component {
           <small className="form__help">
             This should be a one-line call to action for your organization that displays on your landing page.{' '}
             <CharLimit limit={300} string={this.props.organization.description || ''} />
+          </small>
+        </fieldset>
+        <fieldset className="form__fieldset">
+          <label className="form__label" htmlFor="introduction">
+            Introduction
+            <MarkdownEditor
+              className="form__markdown-editor--full"
+              id="introduction"
+              name="introduction"
+              onChange={this.onTextAreaChange}
+              project={this.props.organization}
+              rows="10"
+              value={this.state.textarea}
+            />
+          </label>
+          <small className="form__help">
+            Add a brief introduction to get people interested in your organization.
+            This will display on your landing page.{' '}
+            <CharLimit limit={1500} string={this.state.textarea || ''} />
           </small>
         </fieldset>
       </FormContainer>
