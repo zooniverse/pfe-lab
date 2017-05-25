b91138c2a455a086f81be4c1863bb9ef8a4d0341
srallen <srallen@users.noreply.github.com>
2017-05-25 15:36:32 -0500

    Add node env for oauth and deploys. Setup staging deploy script. Add publisssh (#56)

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,3 +1,14 @@
-export const config = { // eslint-disable-line import/prefer-default-export
-  panoptesAppId: 'a6965f9d7fbe7e921cbf1fc032a02c1b1cc4652cbd6823256a7cc5944ae2c4e1',
+const DEFAULT_ENV = 'staging';
+
+const API_APPLICATION_IDS = {
+  production: '',
+  staging: 'a6965f9d7fbe7e921cbf1fc032a02c1b1cc4652cbd6823256a7cc5944ae2c4e1',
+  development: 'a6965f9d7fbe7e921cbf1fc032a02c1b1cc4652cbd6823256a7cc5944ae2c4e1'
 };
+
+const env = process.env.NODE_ENV || DEFAULT_ENV;
+
+export const config = { // eslint-disable-line import/prefer-default-export
+  panoptesAppId: API_APPLICATION_IDS[env]
+};
+
