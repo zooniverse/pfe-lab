ef0b11a62611bbf42e21164a254dba208effbdf6
James Artz <jelliotartz@users.noreply.github.com>
2017-07-23 23:46:43 -0500

    Add left-nav styling (#73)
    * Add left-nav styling
    
    * Catch up on linting.

--- src/modules/organizations/components/organization-layout.jsx
+++ src/modules/organizations/components/organization-layout.jsx
@@ -1,22 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-const OrganizationLayout = ({ children, navItems, organizationId }) =>
+const OrganizationLayout = ({ children, navItems, organizationId }) => (
   <div className="organization-layout">
     <nav className="organization-layout__nav">
       <ul className="nav-list">
-        {navItems.map(item =>
-          <li key={item.label} className="nav-list__item">
-            <Link to={`/organizations/${organizationId}/${item.to}`}>
+        <li><div className="nav-list-header">Organization #{organizationId}</div></li>
+        <li>
+          <Link
+            to={`/organizations/${organizationId}`}
+            className="view-org-button"
+          >
+            View Organization
+          </Link>
+        </li>
+        {navItems.map(item => (
+          <li key={item.label}>
+            <Link
+              to={`/organizations/${organizationId}/${item.to}`}
+              className="nav-list__item"
+              activeClassName="active"
+            >
               {item.label}
             </Link>
-          </li>,
-        )}
+          </li>
+        ))}
       </ul>
     </nav>
 
     <section className="organization-layout__section">{children}</section>
-  </div>;
+  </div>
+);
 
 OrganizationLayout.propTypes = {
   children: React.PropTypes.node,
@@ -25,12 +39,14 @@ OrganizationLayout.propTypes = {
 };
 
 OrganizationLayout.defaultProps = {
+  children: null,
   navItems: [
-    { to: '', label: 'Edit' },
+    { to: 'edit', label: 'Edit' },
     { to: 'about', label: 'About' },
     { to: 'collaborators', label: 'Collaborators' },
     { to: 'projects', label: 'Projects' },
   ],
+  organizationId: '',
 };
 
 export default OrganizationLayout;
