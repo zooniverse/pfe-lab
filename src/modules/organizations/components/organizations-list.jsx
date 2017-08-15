59d66b0a88d66aaec6b05f4a8a6791ad843681ef
mcbouslog <mcbouslog@gmail.com>
2017-08-15 10:54:20 -0500

    edit view org and help button urls on orgs list

--- src/modules/organizations/components/organizations-list.jsx
+++ src/modules/organizations/components/organizations-list.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router';
+import { config } from '../../../constants/config';
 import ListGroup from './list-group';
 
 import { organizationsAvatarsShape, organizationsShape } from '../model';
 
-const OrganizationsList = ({ createOrganization, collaboratedOrganizations, organizationsAvatars, ownedOrganizations }) => {
+const OrganizationsList = ({
+  createOrganization,
+  collaboratedOrganizations,
+  organizationsAvatars,
+  ownedOrganizations
+}) => {
   if (!ownedOrganizations || !collaboratedOrganizations) {
     return (
       <div>Loading...</div>
@@ -14,7 +19,11 @@ const OrganizationsList = ({ createOrganization, collaboratedOrganizations, orga
   return (
     <div className="organizations-list">
       {ownedOrganizations.length > 0 &&
-        <ListGroup organizations={ownedOrganizations} organizationsAvatars={organizationsAvatars} title={'Your Organizations'} />}
+        <ListGroup
+          organizations={ownedOrganizations}
+          organizationsAvatars={organizationsAvatars}
+          title={'Your Organizations'}
+        />}
       <div className="organizations-list__button-container">
         <button
           type="button"
@@ -23,11 +32,11 @@ const OrganizationsList = ({ createOrganization, collaboratedOrganizations, orga
         >
           Create a new organization
         </button>{' '}
-        <Link to="/" className="button organizations-list__button--info">How-to</Link>{' '}
-        <Link to="/" className="button organizations-list__button--info">Glossary</Link>{' '}
-        <Link to="/" className="button organizations-list__button--info">Policies</Link>{' '}
-        <Link to="/" className="button organizations-list__button--info">Best Practices</Link>{' '}
-        <Link to="/" className="button organizations-list__button--info">Talk</Link>{' '}
+        <a href={`${config.zooniverseURL}help`} className="button organizations-list__button--info">How-to</a>{' '}
+        <a href={`${config.zooniverseURL}help/glossary`} className="button organizations-list__button--info">Glossary</a>{' '}
+        <a href={`${config.zooniverseURL}help/lab-policies`} className="button organizations-list__button--info">Policies</a>{' '}
+        <a href={`${config.zooniverseURL}lab-best-practices/introduction`} className="button organizations-list__button--info">Best Practices</a>{' '}
+        <a href={`${config.zooniverseURL}talk/18`} className="button organizations-list__button--info">Talk</a>{' '}
       </div>
       <hr />
 
